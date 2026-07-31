1e8e0bd48623e64fb9ef3181449efae87ddc0eff
valeriobattipaglia <116173598+valeriobattipaglia@users.noreply.github.com>
2026-07-31 22:01:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ from
 const firebaseConfig = {
   apiKey: "AIzaSyAkHOF2S_NRreXMHU1yDpc6o11r1wpxj9g",
   authDomain: "minifrigo-673a5.firebaseapp.com",
-  projectId: "minifrigo-673a5"
+  projectId: "minifrigo-673a5",
+  storageBucket: "minifrigo-673a5.firebasestorage.app",
+  messagingSenderId: "346725946911",
+  appId: "1:346725946911:web:94d4bda49c1760c315e72a",
+  measurementId: "G-SQBNJC1PNB"
 };
 
 console.log("1 - Script avviato");
