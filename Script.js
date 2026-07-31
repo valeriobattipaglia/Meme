d412d89b5cfb63111767755ad22de5a2ca4e7fe1
valeriobattipaglia <116173598+valeriobattipaglia@users.noreply.github.com>
2026-07-31 16:16:00 +0200

    Initialize Firebase and fetch products from Firestore

---
+++ Script.js
@@ -0,0 +1,42 @@
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getFirestore, collection, getDocs } 
+from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkHOF2S_NRreXMHU1yDpc6o11r1wpxj9g",
+  authDomain: "minifrigo-673a5.firebaseapp.com",
+  projectId: "minifrigo-673a5"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+
+const querySnapshot = await getDocs(
+    collection(db,"prodotti")
+);
+
+
+let html = "";
+
+
+querySnapshot.forEach((doc)=>{
+
+    const prodotto = doc.data();
+
+    html += `
+    <p>
+    ${prodotto.nome}: ${prodotto.quantita}
+    </p>
+    `;
+
+});
+
+
+document.getElementById("lista").innerHTML = html;
